d89bf407812d9a8cfa7f6eaa5bc739a322ad1df2
andrespalmo <andres.palmo@gmail.com>
2017-02-08 00:02:54 -0500

    New file for arduino serial port

--- app2.js
+++ app2.js
@@ -1,24 +1,49 @@
+// Twitter Load
+var TwitterPackage = require('twitter');
+
+var secret = {
+  consumer_key: 'fcxNWolwf2MYKj1tVzz4keB1M',
+  consumer_secret: '2iILrXND0DjEHqLc8xGON6GjDh1jnpZAMtFixSIEICKoU12bWA',
+  access_token_key: '50940458-qKczwpGZQ1UqnuTMaK1r7SBcg3aNOyqqgJ85MKsOj',
+  access_token_secret: 'GhRWBx9OtbgdY0D0aremtPlTH8iVUEsrObvnXh6sQSn91'
+}
+var Twitter = new TwitterPackage(secret);
+
 // serial port initialization:
  var serialport = require('serialport'),
      SerialPort = serialport.SerialPort,
-     portName = '/dev/cu.wchusbserial1420',
+     portName = '/dev/ttyUSB0',
      portConfig = {
          baudRate: 9600,
          // call myPort.on('data') when a newline is received:
          parser: serialport.parsers.readline('\n')
      };
 
-var myPort = new SerialPort(portName, portConfig);
+  var myPort = new SerialPort(portName, portConfig);
 
-//myPort.on('open', openPort); // called when the serial port opens
+  function openPort() {
+      var twitLedAlert = 1;
+      function sendData() {
+           myPort.write(twitLedAlert);
+           console.log('Sending ' + twitLedAlert + ' out the serial port');
+      }
+      setTimeout(sendData, 2000);
+  }
 
-function openPort() {
-    var twitLedAlert = 1;
-    function sendData() {
-         myPort.write(twitLedAlert);
-         console.log('Sending ' + twitLedAlert + ' out the serial port');
-    }
-    setTimeout(sendData, 2000);
-}
+/* Twitter.post('statuses/update', {status: '...'},  function(error, tweet, response){
+  if(error){
+    console.log(error);
+  }
+  console.log(tweet);  // Tweet body.
+  // console.log(response);  // Raw response object.
+}); */
 
-openPort();
+Twitter.stream('statuses/filter', {track: '#8821'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+    openPort();
+  });
+  stream.on('error', function(error) {
+    //console.log(error);
+  });
+});
